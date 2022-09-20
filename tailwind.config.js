584ad17866a53716217b9fa05496145ac4f687d8
Gautam Anand <gautamanand022@gmail.com>
2022-09-20 16:17:32 +0530

    config: bg-card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "card": "url('/src/card-bg.svg')",
+      },
+    },
   },
   plugins: [],
 };
