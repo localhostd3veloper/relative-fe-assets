9bdc4420700470d207841bc22fa65cbdb42bc16e
Gautam Anand <gautamanand022@gmail.com>
2022-09-20 16:18:22 +0530

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import Card from "./Card";
+import { data } from "./static/data";
+
 function App() {
   return (
-    <div className="h-screen bg-[#14172B] flex items-center justify-center text-white"></div>
+    <div className="h-screen bg-[#14172B] flex gap-5 items-center justify-center text-white">
+      {data.map((item) => (
+        <Card
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          tvl={item.tvl}
+          change={item.change}
+          pairs={item.pairs}
+          Icon={item.icon}
+        />
+      ))}
+    </div>
   );
 }
 
